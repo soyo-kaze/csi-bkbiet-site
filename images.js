278495b9c9ff768de115326cdc2baa8b00f35b67
Ghost-kaze <07sameersaini@gmail.com>
2019-07-29 20:53:25 +0530

    some updation gallery and contactus

--- images.js
+++ images.js
@@ -5,16 +5,11 @@ function openCity(tabName) {
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
-
-	// Show the specific tab content
-	document.getElementById(tabName).style.display = "block";
-   
 	//removes background-color of buttons
 	tablinks = document.getElementsByClassName("tablink");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].style.backgroundColor = "";
-	}
+	// Show the specific tab content
+	document.getElementById(tabName).style.display = "block";
 }
 
-	// Get the element with id="defaultOpen" and click on it
-	document.getElementById("defaultOpen").click(); 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click(); 
